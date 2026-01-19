29a85f655d55a6f4d2c6ce6e09ebbc770b4dca92
Grant Chatterton <grantchatterton@gmail.com>
2026-01-19 15:11:27 -0600

    Refactor HomeInfoServiceTimesCol to use Mongoose model for fetching service times

--- src/components/home/info/HomeInfoServiceTimesCol.tsx
+++ src/components/home/info/HomeInfoServiceTimesCol.tsx
@@ -1,16 +1,21 @@
-import { getServiceTimes } from "@/lib/home";
+import dbConnect from "@/lib/mongodb";
+import ServiceTimeModel from "@/models/ServiceTime";
 
 import HomeInfoCol from "./HomeInfoCol";
 
 export default async function HomeInfoServiceTimesCol() {
-  const serviceTimes = await getServiceTimes();
+  await dbConnect();
+  const serviceTimes = await ServiceTimeModel.find()
+    .sort({ startTime: 1 })
+    .lean();
 
   return (
     <HomeInfoCol title="Service Times" md={6}>
       <ul className="list-unstyled mb-0">
         {serviceTimes.map((service) => (
-          <li key={service.id}>
-            {service.name}: {service.startTime} - {service.endTime}
+          <li key={service._id.toString()}>
+            {service.name} ({service.days.join(", ")}): {service.startTime} -{" "}
+            {service.endTime}
           </li>
         ))}
       </ul>
