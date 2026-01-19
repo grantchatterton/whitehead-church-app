509fee2aee998878a7ba34a680fd48b49071f6c5
Grant Chatterton <grantchatterton@gmail.com>
2026-01-19 12:48:48 -0600

    Add Mongoose model for TimelineEvent

--- src/models/TimelineEvent.ts
+++ src/models/TimelineEvent.ts
@@ -1,6 +1,18 @@
-export interface TimelineEvent {
-  id: number;
+import mongoose from "mongoose";
+
+export interface TimelineEvent extends mongoose.Document {
   title: string;
-  date: string;
+  date: Date;
+  dateDisplay?: string;
   description: string;
 }
+
+const TimelineEventSchema = new mongoose.Schema<TimelineEvent>({
+  title: { type: String, required: true },
+  date: { type: Date, required: true },
+  dateDisplay: { type: String, required: false },
+  description: { type: String, required: true },
+});
+
+export default mongoose.models.TimelineEvent ||
+  mongoose.model<TimelineEvent>("TimelineEvent", TimelineEventSchema);
