52afaf9d7d0db549fa10d5c4c63a23260cffed43
Grant Chatterton <grantchatterton@gmail.com>
2026-01-19 14:47:31 -0600

    Add Mongoose model for service times

--- src/models/ServiceTime.ts
+++ src/models/ServiceTime.ts
@@ -1,6 +1,37 @@
-export interface ServiceTime {
-  id: number;
+import "server-only";
+
+import mongoose from "mongoose";
+
+export interface ServiceTime extends mongoose.Document {
   name: string;
-  startTime: string;
-  endTime: string;
+  days: string[]; // e.g., ["Sunday", "Wednesday"]
+  startTime: string; // e.g., "10:00 AM"
+  endTime: string; // e.g., "11:30 AM"
 }
+
+export type ServiceTimeDTO = Pick<
+  ServiceTime,
+  "name" | "days" | "startTime" | "endTime"
+> & { _id: string };
+
+const ServiceTimeSchema = new mongoose.Schema<ServiceTime>({
+  name: { type: String, required: true },
+  days: {
+    type: [String],
+    required: true,
+    enum: [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+  },
+  startTime: { type: String, required: true },
+  endTime: { type: String, required: true },
+});
+
+export default mongoose.models.ServiceTime ||
+  mongoose.model<ServiceTime>("ServiceTime", ServiceTimeSchema);
