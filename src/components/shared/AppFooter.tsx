f667ecc227990a87df98d7f9894e4cf65ec86c6c
Grant Chatterton <grantchatterton@gmail.com>
2026-01-20 14:10:43 -0600

    Update footer to reflect login status and add logout button

--- src/components/shared/AppFooter.tsx
+++ src/components/shared/AppFooter.tsx
@@ -1,6 +1,16 @@
+"use client";
+
+import Link from "next/link";
+
 import Container from "react-bootstrap/Container";
+import Stack from "react-bootstrap/Stack";
+
+import { useSession } from "@/lib/auth-client";
 
 export default function AppFooter({ title }: { title: string }) {
+  const { data: session } = useSession();
+  const user = session?.user;
+
   return (
     <footer className="footer text-center mt-auto">
       <Container>
@@ -16,6 +26,16 @@ export default function AppFooter({ title }: { title: string }) {
             Grant Chatterton
           </a>
         </p> */}
+        {user && (
+          <>
+            <p className="mb-2">
+              Signed in as <strong>{user.email}</strong>
+            </p>
+            <Link href="/logout" className="btn btn-outline-light">
+              Sign Out
+            </Link>
+          </>
+        )}
       </Container>
     </footer>
   );
