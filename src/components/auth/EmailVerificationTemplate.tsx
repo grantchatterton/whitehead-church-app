b4290c253188eb3067b49676b8a41eeab96ff55d
Grant Chatterton <grantchatterton@gmail.com>
2026-01-21 22:17:26 -0600

    Remove header from email verification template for a cleaner design

--- src/components/auth/EmailVerificationTemplate.tsx
+++ src/components/auth/EmailVerificationTemplate.tsx
@@ -5,7 +5,6 @@ export default function EmailVerificationTemplate({
 }) {
   return (
     <>
-      <h1>Verify Your Email Address</h1>
       <p>Please click the link below to verify your email address:</p>
       <a href={verificationUrl}>{verificationUrl}</a>
     </>
