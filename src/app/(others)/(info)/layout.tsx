89958939d9552161d9a00e1685cd62d00b13c596
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 16:15:23 -0600

    Update layout component to specify 'as' prop for Container element

--- src/app/(others)/(info)/layout.tsx
+++ src/app/(others)/(info)/layout.tsx
@@ -2,6 +2,8 @@ import Container from "react-bootstrap/Container";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Container className="my-4 text-center text-md-start">{children}</Container>
+    <Container as="main" className="my-4 text-center text-md-start">
+      {children}
+    </Container>
   );
 }
