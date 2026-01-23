d897e80e1417941c7e701702fce5c1240cc5173d
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 15:53:41 -0600

    Update to not use Container for children

--- src/app/(others)/layout.tsx
+++ src/app/(others)/layout.tsx
@@ -1,14 +1,10 @@
-import Container from "react-bootstrap/Container";
-
 import AppNavbar from "@/components/shared/AppNavbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AppNavbar title="Whitehead Baptist Church" />
-      <Container as="main" className="my-4 text-center text-md-start">
-        {children}
-      </Container>
+      <main>{children}</main>
     </>
   );
 }
