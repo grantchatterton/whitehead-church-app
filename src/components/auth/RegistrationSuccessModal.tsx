66cb09374a9056ed67be26e0792fc10015d6778e
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 16:33:55 -0600

    Add RegistrationSuccessModal

---
+++ src/components/auth/RegistrationSuccessModal.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export default function RegistrationSuccessModal() {
+  const router = useRouter();
+
+  function handleClose() {
+    router.back();
+  }
+
+  return (
+    <Modal show centered backdrop="static" onHide={handleClose}>
+      <Modal.Header>
+        <Modal.Title>Registration Successful</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>Your account has been successfully created.</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Link className="btn btn-outline-light" href="/login">
+          Go to Login
+        </Link>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
