b5b52cf3c1b690f79595e2fe69b1dd7374e1e119
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 14:53:25 -0600

    Add missing order field and timestamps

--- src/models/GalleryImage.ts
+++ src/models/GalleryImage.ts
@@ -9,20 +9,30 @@ export interface GalleryImage extends mongoose.Document {
     title: string;
     description: string;
   };
+  order: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
-export type GalleryImageDTO = Pick<GalleryImage, "src" | "alt" | "caption"> & {
+export type GalleryImageDTO = Pick<
+  GalleryImage,
+  "src" | "alt" | "caption" | "order"
+> & {
   _id: string;
 };
 
-const GalleryImageSchema = new mongoose.Schema<GalleryImage>({
-  src: { type: String, required: true },
-  alt: { type: String, required: true },
-  caption: {
-    title: { type: String, required: true },
-    description: { type: String, required: true },
+const GalleryImageSchema = new mongoose.Schema<GalleryImage>(
+  {
+    src: { type: String, required: true },
+    alt: { type: String, required: true },
+    caption: {
+      title: { type: String, required: true },
+      description: { type: String, required: true },
+    },
+    order: { type: Number, required: true },
   },
-});
+  { timestamps: true }
+);
 
 export default (mongoose.models.GalleryImage as mongoose.Model<GalleryImage>) ||
   mongoose.model<GalleryImage>("GalleryImage", GalleryImageSchema);
