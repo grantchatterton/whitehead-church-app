d2af34a0856d693af93e3283c47bccd107f15474
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 14:41:18 -0600

    Add Mongoose model for GalleryImage

--- src/models/GalleryImage.ts
+++ src/models/GalleryImage.ts
@@ -1,5 +1,8 @@
-export interface GalleryImage {
-  id: number;
+import "server-only";
+
+import mongoose from "mongoose";
+
+export interface GalleryImage extends mongoose.Document {
   src: string; // URL or path to the image
   alt: string;
   caption: {
@@ -7,3 +10,19 @@ export interface GalleryImage {
     description: string;
   };
 }
+
+export type GalleryImageDTO = Pick<GalleryImage, "src" | "alt" | "caption"> & {
+  _id: string;
+};
+
+const GalleryImageSchema = new mongoose.Schema<GalleryImage>({
+  src: { type: String, required: true },
+  alt: { type: String, required: true },
+  caption: {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+});
+
+export default (mongoose.models.GalleryImage as mongoose.Model<GalleryImage>) ||
+  mongoose.model<GalleryImage>("GalleryImage", GalleryImageSchema);
