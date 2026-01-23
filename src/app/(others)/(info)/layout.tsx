4d5ac2e602ea214f0ab6873d12993d9acea4c025
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 15:54:56 -0600

    Update layout for info pages to use Container for children

---
+++ src/app/(others)/(info)/layout.tsx
@@ -0,0 +1,7 @@
+import Container from "react-bootstrap/Container";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Container className="my-4 text-center text-md-start">{children}</Container>
+  );
+}
