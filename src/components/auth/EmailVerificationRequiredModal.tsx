8aa02e34d7ee524613953ea1e0a74f579b1b4617
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 16:24:07 -0600

    Add EmailVerificationRequiredModal

---
+++ src/components/auth/EmailVerificationRequiredModal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+export default function EmailVerificationRequiredModal() {
+  const router = useRouter();
+
+  function handleClose() {
+    router.back();
+  }
+
+  return (
+    <Modal show centered backdrop="static" onHide={handleClose}>
+      <Modal.Header>
+        <Modal.Title>Email Verification Required</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        Please verify your email address to continue. Check your inbox for a
+        verification email.
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="outline-light" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
