84322a4b44d59e3ac28333f8ed51cdbd78900781
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 22:26:08 -0600

    Add user settings page and link to footer (#32)

---
+++ src/app/(others)/user/settings/page.tsx
@@ -0,0 +1,24 @@
+import { headers } from "next/headers";
+
+import Container from "react-bootstrap/Container";
+
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  const user = session?.user;
+  if (!user) {
+    redirect("/login");
+  }
+
+  return (
+    <Container as="main" className="my-4 text-center text-md-start">
+      <h1 className="fs-2">User Settings</h1>
+      <hr />
+      <p>This is the user settings page.</p>
+    </Container>
+  );
+}
