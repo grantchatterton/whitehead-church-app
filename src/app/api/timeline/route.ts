dc2690767b9605dd3903fe79c13dbc5ca6004b0a
Grant Chatterton <grantchatterton@gmail.com>
2026-01-23 20:39:04 -0600

    Remove timeline API route implementation

--- src/app/api/timeline/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-import { getTimelineEvents } from "@/lib/data";
-
-export async function GET() {
-  const data = await getTimelineEvents();
-  return NextResponse.json(data);
-}
