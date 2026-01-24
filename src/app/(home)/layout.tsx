1b080d881ed7883a8c0e93c623d3956511e57776
Grant Chatterton <grantchatterton@gmail.com>
2026-01-24 02:42:09 -0600

    Update layout.tsx

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Container as="main" className="mt-auto">
+    <Container as="main" className="mt-auto mb-4">
       {children}
     </Container>
   );
